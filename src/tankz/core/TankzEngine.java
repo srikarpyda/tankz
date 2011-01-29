7c0436e60d9d7fa0b1cac31d4f73732b940d6126
JimiF3000 <JimiF3000@dc06f3dc-d145-8c13-bda5-bd47d9d04db3>
2011-01-29 22:02:16 +0000

    Set exit conditions for runnable

--- src/tankz/core/TankzEngine.java
+++ src/tankz/core/TankzEngine.java
@@ -5,21 +5,36 @@ import tankz.ui.TankGameUI;
 public class TankzEngine extends Thread {
 	
 	public TankGameUI ui;
+	private boolean isRunning = true;
+	private boolean isPaused = false;
 	
 	public TankzEngine() {
 		ui = new TankGameUI();
 	}
 	
 	public void run() {
-		ui.repaint();
-		iterateLogic();
-		try {
-			sleep(10);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		while(isRunning){
+			while(!isPaused){
+				ui.repaint();
+				iterateLogic();
+				try {
+					sleep(10);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
+	public void stopGame(){
+		this.isRunning = false;
+	}
+	
+	public boolean togglePaused(){
+		this.isPaused = !isPaused;
+		return this.isPaused;
+	}
+	
 	private void iterateLogic() {
 		// TODO Auto-generated method stub
 		
