754d3327755d1da929d8d6971be71356c5d38427
JimiF3000 <JimiF3000@dc06f3dc-d145-8c13-bda5-bd47d9d04db3>
2011-01-30 00:19:20 +0000

    Added support for changing directions

--- src/tankz/core/Tank.java
+++ src/tankz/core/Tank.java
@@ -8,12 +8,18 @@ import javax.imageio.ImageIO;
 
 public class Tank extends ActiveObject {
 	
-	private Image tank;
+	private Image tank_north;
+	private Image tank_east;
+	private Image tank_south;
+	private Image tank_west;
 	
 	public Tank(int x, int y){
 		super(x, y);
 		try{
-			tank = ImageIO.read(new File("images/tank.png"));
+			tank_north = ImageIO.read(new File("images/tank_north.png"));
+			tank_east = ImageIO.read(new File("images/tank_east.png"));
+			tank_south = ImageIO.read(new File("images/tank_south.png"));
+			tank_west = ImageIO.read(new File("images/tank_west.png"));
 		}catch(IOException e){
 			e.printStackTrace();
 			System.exit(1);
@@ -21,7 +27,19 @@ public class Tank extends ActiveObject {
 	}
 
 	public Image getImage() {
-		return tank;
+		switch (this.getDirection()){
+		case NORTH:
+			return tank_north;
+		case EAST:
+			return tank_east;
+		case SOUTH:
+			return tank_south;
+		case WEST:
+			return tank_west;
+		default:
+			return null;
+		}
+		
 	}
 	
 	
